b05e7b2382072d63bd6a6fe81636e82d0e22e9c0
KuppusamyK <kuppusamyk33@gmail.com>
2023-08-10 21:38:46 +0530

    Merge Conflict

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import Login from "./pages/Login";
 
-import ASN from "./pages/ASN";
-import TaskInAssigned from "./pages/TaskInAssigned";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div className="App">
-      {/* <ASN/> */}
-      <TaskInAssigned />
-      {/* <h2>Hello</h2> */}
+      <Register />
+      {/* <Login /> */}
     </div>
   );
 }
