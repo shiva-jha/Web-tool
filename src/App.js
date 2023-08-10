d25094b5200240429b3674cf7ab71aa903b44006
shiva-jha <shivasms2000@gmail.com>
2023-08-10 21:13:28 +0530

    task in assigned status

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from "react";
 
 
 import ASN from "./pages/ASN";
+import TaskInAssigned from "./pages/TaskInAssigned";
  
 
 function App() {
   return (
     <div className="App">
-     <ASN/>
+     {/* <ASN/> */}
+     <TaskInAssigned/>
      {/* <h2>Hello</h2> */}
     </div>
   );
