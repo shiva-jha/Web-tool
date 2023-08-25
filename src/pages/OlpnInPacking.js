56e4afda0eaf27fb88e4ab4ff3e6f32c6ec4bf7b
KuppusamyK <kuppusamyk33@gmail.com>
2023-08-25 23:38:45 +0530

    API and UI Integration

--- src/pages/OlpnInPacking.js
+++ src/pages/OlpnInPacking.js
@@ -8,7 +8,7 @@ const OlpnInPacking = () => {
   const [error, setError] = useState("");
   const [successMessage, setSuccessMessage] = useState("");
   const [openAllocation, setOpenAllocation] = useState("");
-  const [openTasks,setOpenTasks] = useState([]);
+  const [openTasks, setOpenTasks] = useState([]);
 
   const fetchData = async () => {
     setLoading(true);
@@ -17,10 +17,12 @@ const OlpnInPacking = () => {
     setSuccessMessage("");
     setOpenAllocation("");
     setOpenTasks([]);
-
+    const jwtToken = "Bearer " + localStorage.getItem("jwtToken");
 
     try {
-      const response = await axios.post(`http://localhost:7373/checkLPNStatus/${tcRefLpnId}`);
+      const response = await axios.post(
+        `http://localhost:7373/checkLPNStatus/${tcRefLpnId}/${jwtToken}`
+      );
       setData(response.data);
     } catch (error) {
       setError("Error fetching data from the API");
@@ -34,7 +36,9 @@ const OlpnInPacking = () => {
   const handlePacking = async () => {
     if (!isLpnPacked) {
       try {
-        const responseOpenAllocation = await axios.post(`http://localhost:7373/checkOpenAllocationByTcReferenceLpnId/${tcRefLpnId}`);
+        const responseOpenAllocation = await axios.post(
+          `http://localhost:7373/checkOpenAllocationByTcReferenceLpnId/${tcRefLpnId}`
+        );
         if (Array.isArray(responseOpenAllocation.data)) {
           setOpenAllocation(responseOpenAllocation.data);
         } else {
@@ -43,38 +47,53 @@ const OlpnInPacking = () => {
       } catch (error) {
         setOpenAllocation([]);
       }
-  
+
       try {
-        const responseOpenTasks = await axios.post(`http://localhost:7373/checkOpenAllocation/${tcRefLpnId}`);
-        setOpenTasks(responseOpenTasks.data); 
+        const responseOpenTasks = await axios.post(
+          `http://localhost:7373/checkOpenAllocation/${tcRefLpnId}`
+        );
+        setOpenTasks(responseOpenTasks.data);
       } catch (error) {
         setOpenTasks([]);
       }
-  
-      
+
       if (openAllocation.length === 0 && openTasks.length === 0) {
         try {
           // Call three APIs for status checks
-          const responseStatus1 = await axios.post(`http://localhost:7373/checkOrderLineItemByItemId/${tcRefLpnId}`);
-          const responseStatus2 = await axios.post(`http://localhost:7373/checkOrderLineItemByReferenceId/${tcRefLpnId}`);
-          const responseStatus3 = await axios.post(`http://localhost:7373/checkOpenLpnDetails/${tcRefLpnId}`);
-  
-          
+          const responseStatus1 = await axios.post(
+            `http://localhost:7373/checkOrderLineItemByItemId/${tcRefLpnId}`
+          );
+          const responseStatus2 = await axios.post(
+            `http://localhost:7373/checkOrderLineItemByReferenceId/${tcRefLpnId}`
+          );
+          const responseStatus3 = await axios.post(
+            `http://localhost:7373/checkOpenLpnDetails/${tcRefLpnId}`
+          );
+
           if (responseStatus3.data.length > 0) {
-            
-            await axios.post(`http://localhost:7373/updateLpnDetails/${tcRefLpnId}`);
+            await axios.post(
+              `http://localhost:7373/updateLpnDetails/${tcRefLpnId}`
+            );
           }
           if (responseStatus1.data.length > 0) {
-           
-            await axios.post(`http://localhost:7373/updateOrderLineItemUserCancelledQtyLineItemId/${tcRefLpnId}`);
-            await axios.post(`http://localhost:7373/updateOrderLineItemStatusByLineItemId/${tcRefLpnId}`);
+            await axios.post(
+              `http://localhost:7373/updateOrderLineItemUserCancelledQtyLineItemId/${tcRefLpnId}`
+            );
+            await axios.post(
+              `http://localhost:7373/updateOrderLineItemStatusByLineItemId/${tcRefLpnId}`
+            );
           }
           if (responseStatus2.data.length > 0) {
-            
-            await axios.post(`http://localhost:7373/updateOrderLineItemUserCancelledQtyReferenceLineItemId/${tcRefLpnId}`);
-            await axios.post(`http://localhost:7373/updateOrderLineItemStatusByReferenceLineItemId/${tcRefLpnId}`);
+            await axios.post(
+              `http://localhost:7373/updateOrderLineItemUserCancelledQtyReferenceLineItemId/${tcRefLpnId}`
+            );
+            await axios.post(
+              `http://localhost:7373/updateOrderLineItemStatusByReferenceLineItemId/${tcRefLpnId}`
+            );
           }
-          await axios.post(`http://localhost:7373/updateLpnsToPackedStatus/${tcRefLpnId}`);
+          await axios.post(
+            `http://localhost:7373/updateLpnsToPackedStatus/${tcRefLpnId}`
+          );
           setSuccessMessage("Packing completed successfully");
         } catch (error) {
           setError("Error while performing updates");
@@ -82,8 +101,10 @@ const OlpnInPacking = () => {
       }
     }
   };
-  const uniqueTaskIds = Array.from(new Set(openTasks.map(task => task.task_id))); // Get unique task_ids
-  const formattedTaskIds = uniqueTaskIds.join(", "); 
+  const uniqueTaskIds = Array.from(
+    new Set(openTasks.map((task) => task.task_id))
+  ); // Get unique task_ids
+  const formattedTaskIds = uniqueTaskIds.join(", ");
 
   return (
     <div className="container">
@@ -98,7 +119,9 @@ const OlpnInPacking = () => {
             onChange={(e) => setTcRefLpnId(e.target.value)}
           />
         </label>
-        <button className="button" onClick={fetchData}>Fetch Data</button>
+        <button className="button" onClick={fetchData}>
+          Fetch Data
+        </button>
       </div>
 
       {loading && <p>Loading...</p>}
@@ -131,20 +154,31 @@ const OlpnInPacking = () => {
         </div>
       )}
 
-{!isLpnPacked && data.length > 0 && (
+      {!isLpnPacked && data.length > 0 && (
         <div>
-          <button className="button" onClick={handlePacking}>Perform Packing</button>
+          <button className="button" onClick={handlePacking}>
+            Perform Packing
+          </button>
           {successMessage && <h3>{successMessage}</h3>}
           {/* {openAllocation && <p>There is open allocation. Please complete it.</p>} */}
           {openAllocation.length > 0 && (
             <div>
-              <p>There is an open allocation with  IDs: {openAllocation.map(alloc => alloc.alloc_invn_dtl_id).join(", ")}. Please complete them.</p>
+              <p>
+                There is an open allocation with IDs:{" "}
+                {openAllocation
+                  .map((alloc) => alloc.alloc_invn_dtl_id)
+                  .join(", ")}
+                . Please complete them.
+              </p>
             </div>
           )}
           {openTasks.length > 0 && (
             <div>
               {/* <p>There is an open task with task IDs: {openTasks.map(task => task.task_id).join(", ")}. Please complete them.</p> */}
-              <p>There are open tasks with task_ids: {formattedTaskIds}. Please complete the tasks.</p>
+              <p>
+                There are open tasks with task_ids: {formattedTaskIds}. Please
+                complete the tasks.
+              </p>
             </div>
           )}
         </div>
